5b7c48371008fefabef8ed68adb2e09302a9b532
ogullcankara <ogullcankara@gmail.com>
2022-10-05 00:08:05 +0300

    Added margin constrains

--- MarginGuideSpacerViewLab/ViewController.swift
+++ MarginGuideSpacerViewLab/ViewController.swift
@@ -11,11 +11,70 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
         
     }
 
+    func setupViews(){
+        
+        let leadingGuide = UILayoutGuide()
+        let okButton = makeButton(title: "Ok", color: UIColor.darkBlue)
+        let middleGuide = UILayoutGuide()
+        let cancelButton = makeButton(title: "Cancel", color: UIColor.darkGreen)
+        let traillingGuide = UILayoutGuide()
+        
+        view.addSubview(okButton)
+        view.addSubview(cancelButton)
+        view.addLayoutGuide(leadingGuide)
+        view.addLayoutGuide(middleGuide)
+        view.addLayoutGuide(traillingGuide)
+        
+        let margins = view.layoutMarginsGuide
+        //leading guide
+        margins.leadingAnchor.constraint(equalTo: leadingGuide.leadingAnchor).isActive = true
+        leadingGuide.trailingAnchor.constraint(equalTo: okButton.leadingAnchor).isActive = true
+        //middle guide
+        okButton.trailingAnchor.constraint(equalTo: middleGuide.leadingAnchor).isActive = true
+        middleGuide.trailingAnchor.constraint(equalTo: cancelButton.leadingAnchor).isActive = true
+        //trailling guide
+        cancelButton.trailingAnchor.constraint(equalTo: traillingGuide.leadingAnchor).isActive = true
+        traillingGuide.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        //button equal widths
+        okButton.widthAnchor.constraint(equalTo: cancelButton.widthAnchor).isActive = true
+        //spacer equal widths
+        leadingGuide.widthAnchor.constraint(equalTo: middleGuide.widthAnchor).isActive = true
+        leadingGuide.widthAnchor.constraint(equalTo: traillingGuide.widthAnchor).isActive = true
+        //vertical position
+        leadingGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        middleGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        traillingGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        okButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        cancelButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        //giving layout guides default heigt
+        leadingGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        middleGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        traillingGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+    }
     
+    func makeButton(title: String, color: UIColor) -> UIButton{
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.contentEdgeInsets = UIEdgeInsets.init(top: 8, left: 16, bottom: 8, right: 16)
+        //button.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+        button.backgroundColor = color
+        
+        
+        return button
+    }
     
 
 }
 
+extension UIColor {
+    static let darkBlue = UIColor(red: 10/255, green: 132/255, blue: 255/255, alpha: 1)
+    static let darkGreen = UIColor(red: 48/255, green: 209/255, blue: 88/255, alpha: 1)
+}
